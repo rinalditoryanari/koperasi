b3706e8f76e2fc3f5c722140929a0612639392c7
Rinaldito <rinalditorynari@gmail.com>
2022-08-28 12:39:19 +0700

    semoga bisa

---
+++ application/controllers/C_akunMaster.php
@@ -0,0 +1,120 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class C_akunMaster extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+        $this->load->library('form_validation');
+        $this->load->model('M_akunMaster');
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata("akun_id") == "") {
+            $this->session->set_flashdata('flash3', 'Login terlebih dahulu');
+            redirect(site_url('CLogin'));
+        } else {
+            $data['all_akun']   = $this->M_akunMaster->index();
+
+            $this->load->view('template/header');
+            $this->load->view('template/vsidebar');
+            $this->load->view('akun/vakun', $data);
+            $this->load->view('template/footer');
+        }
+    }
+
+    public function alluser()
+    {
+        if ($this->session->userdata("akun_id") == "") {
+            $this->session->set_flashdata('flash3', 'Login terlebih dahulu');
+            redirect(site_url('CLogin'));
+        } else {
+            $data['all_akun']   = $this->M_akunMaster->alluser();
+
+            $this->load->view('template/header');
+            $this->load->view('template/vsidebar');
+            $this->load->view('akun/vakun', $data);
+            $this->load->view('template/footer');
+        }
+    }
+
+    public function form_akun()
+    {
+        if ($this->session->userdata("akun_id") == "") {
+            $this->session->set_flashdata('flash3', 'Login terlebih dahulu');
+            redirect(site_url('CLogin'));
+        } else {
+            $data['list_nelayan'] = $this->M_akunMaster->list_nelayan();
+            $this->load->view('template/header');
+            $this->load->view('template/vsidebar');
+            $this->load->view('akun/vform_akun', $data);
+            $this->load->view('template/footer');
+        }
+    }
+
+    public function tambah_akun()
+    {   
+        $tipe = 2;
+        $id_nelayan = "";
+        $data = array(
+            'akun_id'            => $this->input->post('akun_id'),
+            'username'          => $this->input->post('username'),
+            'password'         => $this->input->post('password'),
+            'code'         => $this->input->post('code'),
+            'tipe'         => $tipe,
+            'id_nelayan'         => $id_nelayan,
+            'asal'     => $this->input->post('asal'),
+           
+
+        );
+        $query = $this->db->insert('akun', $data);
+        if ($query = true) {
+            $this->session->set_flashdata('info', 'Data Berhasil Di Simpan');
+            redirect('C_akunMaster');
+        }
+    }
+
+    public function edit($akun_id)
+    {
+        if ($this->session->userdata("akun_id") == "") {
+            $this->session->set_flashdata('flash3', 'Login terlebih dahulu');
+            redirect(site_url('CLogin'));
+        } else {
+            $isi['data'] = $this->M_akunMaster->edit($akun_id);
+            $isi['list_nelayan'] = $this->M_akunMaster->list_nelayan();
+            $this->load->view('template/header');
+            $this->load->view('template/vsidebar');
+            $this->load->view('akun/vformedit_akun', $isi);
+            $this->load->view('template/footer');
+        }
+    }
+    public function update()
+    {
+        $akun_id = $this->input->post('akun_id');
+        $id_nelayan = "";
+        $tipe = 2;
+        $data = array(
+            'akun_id'                => $this->input->post('akun_id'),
+            'username'              => $this->input->post('username'),
+            'password'        => $this->input->post('password'),
+            'code'        => $this->input->post('code'),
+            'tipe'        => $tipe,
+            'id_nelayan'        => $id_nelayan,
+
+        );
+        $query = $this->M_akunMaster->update($akun_id, $data);
+        if ($query = true) {
+            $this->session->set_flashdata('info', 'Data Berhasil Di Update');
+            redirect('C_akunMaster');
+        }
+    }
+    public function hapus_akun($id)
+    {
+        $a = $this->M_akunMaster->hapus_akun($id);
+        redirect('C_akunMaster');
+    }
+}
---
+++ application/models/M_akunMaster.php
@@ -0,0 +1,154 @@
+<?php
+
+use JetBrains\PhpStorm\Internal\ReturnTypeContract;
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class M_akunMaster extends ci_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+    }
+
+    public function index($isall = true, $limit = null, $offset = null)
+    {
+        if ($this->session->userdata('client_id')) {
+            $ses_client = $this->session->userdata('client_id');
+        } else {
+            $ses_client = $this->session->userdata('ID');
+        }
+
+        $keyword = str_replace("'", "\'", $this->input->get('table_search'));
+
+        $where = array();
+        if (!empty($this->input->get('table_search'))) {
+            $where[] = " AND username LIKE '%" . $keyword . "%'";
+        }
+
+        $is_limit = true;
+
+        isLimit:
+
+        $stringwhere = implode(" AND ", $where);
+        $lokasi = $this->session->userdata('asal');
+
+        $query = "  SELECT
+                            a.`akun_id`, a.`username`, a.`password`, a.`code`, a.`tipe`,a.`asal`, b.`nama` as nama_nelayan
+                        FROM `akun` a
+                        LEFT JOIN `nelayan` b on a.`id_nelayan` = b.`id`
+                        where a.`tipe`= '2'
+                        $stringwhere 
+                        ORDER BY a.`akun_id` DESC;";
+
+        // echo $query;
+        // die();
+        if ($is_limit) {
+            if (!$isall) {
+                $query .= ' LIMIT ' . $limit . " offset " . $offset;
+            }
+        }
+
+        if ($is_limit) {
+            $res = $this->db->query($query)->result_array();
+            $is_limit = false;
+            goto isLimit;
+        }
+
+        $count = $this->db->query($query)->num_rows();
+
+        $data = array(
+            "total" => $count,
+            "data" => $res
+        );
+        // var_dump($data);
+        // die;
+        return $data;
+    }
+
+    public function alluser($isall = true, $limit = null, $offset = null)
+    {
+        if ($this->session->userdata('client_id')) {
+            $ses_client = $this->session->userdata('client_id');
+        } else {
+            $ses_client = $this->session->userdata('ID');
+        }
+
+        $keyword = str_replace("'", "\'", $this->input->get('table_search'));
+
+        $where = array();
+        if (!empty($this->input->get('table_search'))) {
+            $where[] = " AND username LIKE '%" . $keyword . "%'";
+        }
+
+        $is_limit = true;
+
+        isLimit:
+
+        $stringwhere = implode(" AND ", $where);
+        $lokasi = $this->session->userdata('asal');
+
+        $query = "  SELECT
+                            a.`akun_id`, a.`username`, a.`password`, a.`code`, a.`tipe`,a.`asal`, b.`nama` as nama_nelayan
+                        FROM `akun` a
+                        LEFT JOIN `nelayan` b on a.`id_nelayan` = b.`id`
+                        where a.`tipe`!= '2' AND a.`tipe`!= '3'
+                        $stringwhere 
+                        ORDER BY a.`akun_id` DESC;";
+
+        // echo $query;
+        // die();
+        if ($is_limit) {
+            if (!$isall) {
+                $query .= ' LIMIT ' . $limit . " offset " . $offset;
+            }
+        }
+
+        if ($is_limit) {
+            $res = $this->db->query($query)->result_array();
+            $is_limit = false;
+            goto isLimit;
+        }
+
+        $count = $this->db->query($query)->num_rows();
+
+        $data = array(
+            "total" => $count,
+            "data" => $res
+        );
+        // var_dump($data);
+        // die;
+        return $data;
+    }
+
+    public function hapus_akun($akun_id)
+    {
+        $querylog1   = "DELETE FROM akun WHERE akun_id='$akun_id';";
+        $run2       = $this->db->query($querylog1);
+      
+        // $this->session->set_flashdata('flash', 'Berhasil Dihapus');
+    }
+    public function edit($akun_id)
+    {
+        $this->db->where('akun_id', $akun_id);
+        return $this->db->get('akun')->row_array();
+    }
+    public function update($akun_id, $data)
+    {
+        $this->db->where('akun_id', $akun_id);
+        $this->db->update('akun', $data);
+    }
+
+    public function list_nelayan()
+    {
+        $pilih_client = "SELECT	
+                            `id` as id_nelayan,
+                            `nama` as nama_nelayan,
+                            `nama_kapal` as kapal_nelayan
+                        FROM `nelayan`
+                        WHERE `status` = 1";
+        $client = $this->db->query($pilih_client)->result_array();
+
+        return $client;
+    }
+}
