8f7b74b4e2b04b0846af187d6c9baf2c5d79881f
Raditya <radityaarya53@gmail.com>
2022-10-18 20:44:13 +0700

    tampilan

--- application/views/vhomepage.php
+++ application/views/vhomepage.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Sumber Laut Mandiri</title>
+        <title>Koperasi Nelayan Indonesia</title>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
@@ -13,13 +13,14 @@
         <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="assets/css/styles.css" rel="stylesheet" />
+        <link href="assets/css/home.css" rel="stylesheet" />
+        <link href="assets/css/styles.css" rel="stylesheet" />     
     </head>
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
             <div class="container px-5">
-                <a class="navbar-brand" href="#page-top">Koperasi Sumber Laut Mandiri</a>
+                <a class="navbar-brand" href="#page-top">Koperasi Nelayan Indonesia</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
@@ -30,9 +31,29 @@
         </nav>
         <!-- Header-->
       
+        
+        <section id="scroll">
+            <div class="container nelayan px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/nelayan.png"  /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">Nelayan</h2>
+                            <p> Istilah bagi orang-orang yang sehari-harinya bekerja menangkap ikan atau biota lainnya yang hidup di dasar, kolom maupun permukaan perairan. 
+                                Perairan yang menjadi daerah aktivitas nelayan ini dapat merupakan perairan tawar, payau maupun laut. Di negara-negara berkembang seperti di 
+                                Asia Tenggara atau di Afrika, masih banyak nelayan yang menggunakan peralatan yang sederhana dalam menangkap ikan. Nelayan di negara-negara maju 
+                                biasanya menggunakan peralatan modern dan kapal yang besar yang dilengkapi teknologi canggih.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
         <!-- Content section 1-->
         <section id="scroll">
-            <div class="container px-5">
+            <div class="container  px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-2">
                         <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/nelayan.png"  /></div>
@@ -51,7 +72,7 @@
         </section>
         <!-- Content section 2-->
         <section>
-            <div class="container px-5">
+            <div class="container  px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6">
                         <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/perahu.png"  /></div>
@@ -68,7 +89,7 @@
         </section>
         <!-- Content section 3-->
         <section>
-            <div class="container px-5">
+            <div class="container  px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-2">
                         <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/jaring.png"  /></div>
@@ -83,9 +104,104 @@
                 </div>
             </div>
         </section>
+
+        <!-- Content section PNJ -->
+        <section>
+            <div class="container px-5">
+                <div class="row align-items-center">
+                    <h1 class="text-center py-5">About Us</h1>
+                    <div class="col-lg-6">
+                        <div class="p-5"><img class="foto-pnj rounded-circle" src="gambar/politeknik.png"/></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <h2 class="display-5 text-center">Politeknik Negeri Jakarta</h2>
+                            <p class="text-justify">Politeknik Negeri Jakarta merupakan lembaga pendidikan tinggi Diploma III di Jakarta yang didirikan untuk memenuhi kebutuhan SDM profesional di industri, baik industri jasa maupun industri manufaktur.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content section Tim Peneliti Dosen -->
+        <section>
+            <div class="container px-5 mt-5">
+                <div class="row">
+                    <h1 class="text-center display-3 py-5"> Dosen Peneliti</h1>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Mera Kartika Delimayanti, S.Si., M.T., Ph.D</p>
+                            </div>
+                            <!-- <div class="px-4">
+                                <p class="text-justify">Politeknik Negeri Jakarta merupakan lembaga pendidikan tinggi Diploma III di Jakarta yang didirikan untuk memenuhi kebutuhan SDM profesional di industri, baik industri jasa maupun industri manufaktur.</p>
+                            </div> -->
+                    </div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Utami Puji Lestari, S.E., M.Ak., Ph.D</p>
+                            </div>
+                            <!-- <div class="px-4">
+                                <p class="text-justify">Politeknik Negeri Jakarta merupakan lembaga pendidikan tinggi Diploma III di Jakarta yang didirikan untuk memenuhi kebutuhan SDM profesional di industri, baik industri jasa maupun industri manufaktur.</p>
+                            </div> -->
+                    </div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Dra Wiwi Prastiwinarti M.M.</p>
+                            </div>
+                            <!-- <div class="px-4">
+                                <p class="text-justify">Politeknik Negeri Jakarta merupakan lembaga pendidikan tinggi Diploma III di Jakarta yang didirikan untuk memenuhi kebutuhan SDM profesional di industri, baik industri jasa maupun industri manufaktur.</p>
+                            </div> -->
+                        </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Content section tim peneliti mahasiswa -->
+        <section>
+            <div class="container px-5 mt-5">
+                <div class="row">
+                    <h1 class="text-center display-3 py-5">Tim Peneliti Mahasiswa</h1>
+                    <div class="col-lg-6 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Rizaldi Akbar</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-6 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Rhaka</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Muhammad Khairudin Ismail</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Rinaldito Ahmad Ryanari</p>
+                            </div>
+                    </div>
+                    <div class="col-lg-4 d-flex flex-column">
+                        <div class="d-flex align-items-center justify-content-center mb-2"><img class="foto-profile rounded-circle" src="gambar/politeknik.png"/></div>
+                            <div class="p-3 nama-dosen">
+                                <p class="text-center h4">Raditya Arya Prasetyo </p>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </section>
         <!-- Footer-->
         <footer class="py-5 bg-black">
-            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Koperasi Sumber Laut Mandiri</p></div>
+            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Politeknik Negeri Jakarta 2022</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
